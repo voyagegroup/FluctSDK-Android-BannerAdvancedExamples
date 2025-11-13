4d3a6efdcd5862642e0e1d96bf457ea3dba75b4d
voyagegroup <fluct_dev@cartahd.com>
2025-11-13 11:06:26 +0000

    Update

--- BannerAdvancedExamples/app/src/main/java/jp/fluct/banneradvancedexamples/MainActivity.kt
+++ BannerAdvancedExamples/app/src/main/java/jp/fluct/banneradvancedexamples/MainActivity.kt
@@ -5,7 +5,6 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import jp.fluct.banneradvancedexamples.databinding.MainActivityBinding
-import jp.fluct.fluctsdk.FluctAdRequestTargeting
 import jp.fluct.fluctsdk.FluctAdSize
 import jp.fluct.fluctsdk.FluctAdView
 import jp.fluct.fluctsdk.FluctErrorCode
@@ -24,12 +23,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater).apply { setContentView(this.root) }
-
-        // ターゲティング設定
-        val targeting = FluctAdRequestTargeting().apply {
-            // ユーザIDを設定してください
-            this.publisherProvidedId = "c6c506f327740abe750e05cd8d0dc22414d173c43814409c70e722ef2d124806360aaf3146c63226a4e2c862ebdd11b83d37b7b6e144ab4619bca8a0b22d054e"
-        }
 
         // 親ViewGroupサイズに従い比率計算を行う場合、ViewTreeObserverの使用を推奨します
         binding.container.viewTreeObserver.addOnGlobalLayoutListener {
@@ -53,7 +46,7 @@ class MainActivity : AppCompatActivity() {
                 "1000245600",
                 (adWidth/density).roundToInt(),
                 (adHeight/density).roundToInt(),
-                targeting,
+                null,
                 listener
             )
             binding.container.addView(adView)
